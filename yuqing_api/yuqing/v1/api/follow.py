d3fcc8d22415cc304ce6fdfdfb4d78f0e7071951
shenbin <515952853@qq.com>
2018-04-19 16:27:40 +0800

    update(follow)

--- yuqing_api/yuqing/v1/api/follow.py
+++ yuqing_api/yuqing/v1/api/follow.py
@@ -23,25 +23,26 @@ class Follow(ApiHandler):
                     dbcompanyinfo.is_follow = 1
                     dbcompanyinfo.follow_time = datetime.now()
                     dbsession.add(dbcompanyinfo)
-                    dbsession.commit()
                 else:     
                     dbcompanyinfo = follow_company(employee_id=user.employee.id,company_id= id)
                     dbsession.add(dbcompanyinfo)
-                    dbsession.commit()
+                dbemployeefollow = dbsession.query(employee_follow).filter(employee_follow.id == user.employee.id).one_or_none()
+                dbemployeefollow.company_count = dbemployeefollow.company_count+1
+                dbsession.add(dbemployeefollow)
+                dbsession.commit()
             if follow_type == 2:
                 dbindustryinfo = dbsession.query(follow_industry).filter(and_(follow_industry.employee_id == user.employee.id,follow_industry.industry_id == id)).one_or_none()
                 if dbindustryinfo:
                     dbindustryinfo.is_follow = 1
                     dbindustryinfo.follow_time = datetime.now()
                     dbsession.add(dbindustryinfo)
-                    dbsession.commit()
                 else:     
                     dbindustryinfo = follow_company(employee_id=user.employee.id,industry_id= id)
                     dbsession.add(dbindustryinfo)
-                    dbsession.commit()
-
-        
-
+                dbemployeefollow = dbsession.query(employee_follow).filter(employee_follow.id == user.employee.id).one_or_none()
+                dbemployeefollow.industry_count = dbemployeefollow.instury_count+1
+                dbsession.add(dbemployeefollow)
+                dbsession.commit()
         return None, 204, None
 
     def delete(self):
@@ -51,15 +52,21 @@ class Follow(ApiHandler):
         if user.valid:
             dbsession = _connectDBdata_()
             if follow_type == 1:
-                dbcompanyinfo = dbsession.query(follow_company).filter(and_(follow_company.employee_id == user.employee.id),follow_company.company_id == id).one()      
+                dbcompanyinfo = dbsession.query(follow_company).filter(and_(follow_company.employee_id == user.employee.id),follow_company.company_id == id).one_or_none()      
                 dbcompanyinfo.is_follow = 0
                 dbcompanyinfo.unfollow_time = datetime.now()
                 dbsession.add(dbcompanyinfo)
+                dbemployeefollow = dbsession.query(employee_follow).filter(employee_follow.id == user.employee.id).one_or_none()
+                dbemployeefollow.company_count = dbemployeefollow.company_count-1
+                dbsession.add(dbemployeefollow)
                 dbsession.commit()
             if follow_type == 2:
-                dbindustryinfo = dbsession.query(follow_industry).filter(and_(follow_industry.employee_id == user.employee.id,follow_industry.industry_id == id)).one()
+                dbindustryinfo = dbsession.query(follow_industry).filter(and_(follow_industry.employee_id == user.employee.id,follow_industry.industry_id == id)).one_or_none()
                 dbindustryinfo.is_follow = 0
                 dbindustryinfo.unfollow_time = datetime.now()
                 dbsession.add(dbindustryinfo)
+                dbemployeefollow = dbsession.query(employee_follow).filter(employee_follow.id == user.employee.id).one_or_none()
+                dbemployeefollow.industry_count = dbemployeefollow.instury_count-1
+                dbsession.add(dbemployeefollow)
                 dbsession.commit()
         return None, 204, None
