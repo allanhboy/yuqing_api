ba9685fcb8699bb646904bde12a43821748ab428
shenbin <515952853@qq.com>
2018-04-23 15:13:29 +0800

    follow数量获取接口实现

--- yuqing_api/yuqing/v1/api/follow_state.py
+++ yuqing_api/yuqing/v1/api/follow_state.py
@@ -4,9 +4,17 @@ from __future__ import absolute_import, print_function
 from . import ApiHandler
 from .. import schemas
 
+from core.PackageDB import employee_follow, _connectDBdata_
 
 class FollowState(ApiHandler):
 
     def get(self):
-
-        return {}, 200, None
+        user = self.get_current_user()
+        if not user.valid:
+            return None ,401,None
+        if user.employee.id is None:
+            return None ,403,None
+        dbsession = _connectDBdata_()
+        dbemployeefollow = dbsession.query(employee_follow).filter(employee_follow.id == user.employee.id).one_or_none()
+        response = {'company':dbemployeefollow.company_count,'industry':dbemployeefollow.industry_count}
+        return response, 200, None
